e1d66b95a2a63f1da56e11744b38b06faa287cab
bachtuyetx7chulun <contact.truongtrunghieu@gmail.com>
2021-06-02 00:19:48 +0700

    upate

--- server/src/main.js
+++ server/src/main.js
@@ -2,8 +2,12 @@ import express from 'express'
 import { getError, handleError } from './utils/error.handle.js'
 import router from './routers/index.js'
 import endPoints from 'express-list-endpoints'
+import cors from 'cors'
+import helmet from 'helmet'
 
 let app = express()
+app.use(cors())
+app.use(helmet())
 
 app.use('/api/v1/', router)
 app.use(getError)
