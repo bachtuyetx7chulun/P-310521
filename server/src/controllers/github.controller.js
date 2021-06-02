af683074e06c84bc7437f487410dfba9a1e726d3
bachtuyetx7chulun <contact.truongtrunghieu@gmail.com>
2021-06-03 00:45:11 +0700

    update to github controller

--- server/src/controllers/github.controller.js
+++ server/src/controllers/github.controller.js
@@ -2,17 +2,34 @@ import axios from 'axios'
 import { GITHUB_OWNER, GITHUB_URL } from '../configs/variables.config.js'
 import { ApiError } from '../utils/error.handle.js'
 
+export const getUser = async (req, res, next) => {
+    const url = `${GITHUB_URL}users/${GITHUB_OWNER}`
+    try {
+        const { data } = await axios.get(url, {
+            headers: {
+                Accept: 'application/vnd.github.v3+json'
+            }
+        })
+
+        return res.status(200).json({
+            data
+        })
+    } catch (error) {
+        next(new ApiError('Bad request', 400))
+    }
+}
+
 export const getReps = async (req, res, next) => {
     const url = `${GITHUB_URL}users/${GITHUB_OWNER}/repos`
     try {
         const { data } = await axios.get(url, {
             headers: {
-                Accept: 'application/vnd.github.v3+json',
-            },
+                Accept: 'application/vnd.github.v3+json'
+            }
         })
 
         return res.status(200).json({
-            data,
+            data
         })
     } catch (error) {
         next(new ApiError('Bad request', 400))
@@ -25,12 +42,12 @@ export const getDetailRep = async (req, res, next) => {
     try {
         const { data } = await axios.get(url, {
             headers: {
-                Accept: 'application/vnd.github.v3+json',
-            },
+                Accept: 'application/vnd.github.v3+json'
+            }
         })
 
         return res.status(200).json({
-            data,
+            data
         })
     } catch (error) {
         next(new ApiError('Bad request', 400))
